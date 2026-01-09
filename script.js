3c157c01108d382db27131bf16b8223959dde000
Gustavo Soares <gustavo.soares220@hotmail.com>
2026-01-09 11:30:30 -0300

    Testando selectors

---
+++ script.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById("animais");
+
+console.log(animais);
+
+const gridSection = document.getElementsByClassName("grid-section");
+
+console.log(gridSection);
+
+const primeiraLi = document.querySelector("li");
+console.log(primeiraLi);
+
+const linksInternos = document.querySelector('[href^="#"]');
+console.log(linksInternos);
+
+const animaisIMG = document.querySelectorAll(".animais img");
+
+console.log(animaisIMG);
