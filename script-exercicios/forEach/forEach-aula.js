53c93a8c8501dadda04dbd5413058294bc3c560e
Gustavo Soares <gustavo.soares220@hotmail.com>
2026-01-09 12:32:46 -0300

    Ecercícios forEach

---
+++ script-exercicios/forEach/forEach-aula.js
@@ -0,0 +1,9 @@
+const imgs = document.querySelectorAll("img");
+
+// imgs.forEach(function (item, index) {
+//   console.log(index, item);
+// });
+
+imgs.forEach((item, index) => {
+  console.log(item);
+});
--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-// Retorne no console todas as imagens do site
-const imagens = document.querySelectorAll("img");
-console.log(imagens);
-
-// Retorne no console apenas as imagens que começaram com a palavra imagem
-const imagensPalavra = document.querySelectorAll('img[src^="img/imagem"]');
-console.log(imagensPalavra);
-
-// Selecione todos os links internos (onde o href começa com #)
-const linkInterno = document.querySelectorAll('[href^="#"]');
-console.log(linkInterno);
-
-// Selecione o primeiro h2 dentro de .animais-descricao
-const h2Animais = document.querySelector(".animais-descricao h2 ");
-console.log(h2Animais);
-
-// Selecione o último p do site
+// Mostre no console cada parágrado do site
 const paragrafos = document.querySelectorAll("p");
-console.log(paragrafos[--paragrafos.length]);
+
+paragrafos.forEach((item) => {
+  console.log(item);
+});
+
+// Mostre o texto dos parágrafos no console
+
+paragrafos.forEach((item) => {
+  console.log(item.innerText);
+});
+// Como corrigir os erros abaixo:
+
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
